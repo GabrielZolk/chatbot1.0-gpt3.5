06d6f18d1676f13ab20873083ebd1126ea3f86c6
GabrielZolk <dialixkat@gmail.com>
2023-07-27 21:01:56 -0300

    sketch

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('Hello World')
+import { Message, Whatsapp, create } from 'venom-bot';
+
+create({
+    session: 'chatbot',
+    disableWelcome: true,
+})
+  .then(async (client: Whatsapp) => await start(client))
+  .catch((err) => {
+    console.log(err)
+  })
+
+async function start(client: Whatsapp) {
+    client.onMessage(async (message: Message) => {
+        if (!message.body || message.isGroupMsg) return
+
+        console.log('Message: ' + message.body)
+        const response = `Olá!`
+
+        await client.sendText(message.from, response)
+    })
+}
